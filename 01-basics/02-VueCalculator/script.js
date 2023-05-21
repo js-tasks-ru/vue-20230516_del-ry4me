816a3d520f754d2fa10c89ea1cb58cb0c271ed24
Artem Kuteynikov <del-ry4me@yandex.ru>
2023-05-21 21:50:11 +0300

    solution for 01-basics

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,40 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 // Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: ''
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'sum':
+          return this.num1 + this.num2;
+          break;
+        case 'multiply':
+          return this.num1 * this.num2;
+          break;
+        case 'subtract':
+          return this.num1 - this.num2;
+          break;
+        case 'divide':
+          return this.num1 / this.num2;
+           break;
+        default:
+          return 0;
+      }
+
+    }
+  },
+
+});
+
+const app = createApp(App)
+const vm = app.mount('#app')
--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+// Создайте Vue приложение
+
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      radioButton: 0,
+      title: ''
+    };
+  },
+
+  watch: {
+    radioButton(newVal, oldVal) {
+      this.fetchMeetupById(newVal).then((json) => this.title = json.title);
+
+    },
+  },
+
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
     }
-  });
-}
+  },
 
-// Требуется создать Vue приложение
+});
+
+const app = createApp(App)
+const vm = app.mount('#app')
+
